c9f13eb7b77c59e5c6b6bfbc02fc3f57e1449ec7
kaba <taikikabaya@gmail.com>
2021-07-21 22:53:22 +0900

    clientをtry catchの外側に出した。；を削除

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const app = express()
 
 // queryを投げるだけなので、全てgetで大丈夫かな？
 app.get('/', async (req, res) => {
+	const client = await new Client({
+		host: 'ec2-35-171-250-21.compute-1.amazonaws.com',
+		user: 'yrukxmdgnsxqlw',
+		database: 'd2uuqa38bme5q9',
+		password: '4f08f2cb878b021a31494040159fb41732d32b652268604b646f480fdc6de57b',
+		port: 5432,
+		// ローカルからherokuのpostgrelqlに接続するため自己証明書を有効にしている。
+		// 御行儀がよくないため、時間がある時に修正する。
+		ssl: { rejectUnauthorized: false }
+	})
 	try {
-		const client = await new Client({
-			host: 'ec2-35-171-250-21.compute-1.amazonaws.com',
-			user: 'yrukxmdgnsxqlw',
-			database: 'd2uuqa38bme5q9',
-			password: '4f08f2cb878b021a31494040159fb41732d32b652268604b646f480fdc6de57b',
-			port: 5432,
-			// ローカルからherokuのpostgrelqlに接続するため自己証明書を有効にしている。
-			// 御行儀がよくないため、時間がある時に修正する。
-			ssl: { rejectUnauthorized: false }
-		})
 		await client.connect()
 
 		const param = req.query
@@ -30,12 +30,12 @@ app.get('/', async (req, res) => {
 		.status(200)
 		.set('Content-Type', 'text/plain')
 		.send(response.rows)
-		.end();
+		.end()
 	} catch {
 		res.status(500)
 		.set('Content-Type', 'text/plain')
 		.send(null)
-		.end();
+		.end()
 	} finally {
 		client.end() // なぜclient.endの代わりにclient.releaseを使用した方が良いかまだ調べていない。
 	}
