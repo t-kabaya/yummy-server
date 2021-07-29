207a7f2fe00171cdaf75ceba0472e8cf179443d6
kaba <taikikabaya@gmail.com>
2021-07-30 00:50:38 +0900

    init

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const pg = require('pg')
+const pgp = require("pg-promise")()
 const env = require('./env')
 const { query } = require('./execQuery')
 
-const { Client } = pg
-pg.defaults.ssl = true // sslで接続
+pgp.pg.defaults.ssl = {
+	rejectUnauthorized: false // 自己証明書を有効に変更
+}
+
 
 exports.execQuery = async (req, res) => {
 	const sql = req.query.sql
 	const data = JSON.parse(req.query.data) // やや危険な実装。
-	const client = await new Client({
+
+	const client = pgp({
 		host: env.host,
 		user: env.user,
 		database: env.database,
 		password: env.password,
 		port: env.port,
-		// ローカルからherokuのpostgrelqlに接続するため自己証明書を有効にしている。
-		// 御行儀がよくないため、時間がある時に修正する。
-		ssl: { rejectUnauthorized: false }
 	})
 	try {
 		await client.connect()
-		const response = await client.query(sql, data)
+		const response = await client.any(sql, data)
 		res.status(200).send(response.rows)
 	} catch(e) {
-		await client.query('ROLLBACK')
 		res.status(500).send(null)
 	} finally {
 		client.end()
